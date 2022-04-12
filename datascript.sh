af3a3490a24ab728ec73cc64776aef79e092c08f
Victor Alvarez <91897997+vapuppet@users.noreply.github.com>
2022-04-12 18:08:29 -0400

    Create datascript.sh

---
+++ datascript.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#1. High Level Overview
+echo Customer name:
+read name
+echo Customer email:
+read email
+echo Customer name: $name >> output.txt
+echo Customer email: $email >> output.txt
+echo Puppet Enterprise Version:
+puppet --version | tee -a output.txt
+
+#2. Support Script Generation
+#
+#
+
+#3. PE Server Commands
+/opt/puppetlabs/puppet/bin/openssl x509 -in "$(/opt/puppetlabs/bin/puppet config print hostcert)" --enddate --noout | tee -a output.txt
+echo
+echo Puppet Infrastructure Status | tee -a output.txt
+puppet infra status | tee -a output.txt
+echo
+echo Puppet Module List | tee -a output.txt
+echo "puppet module list --all" >> output.txt
+puppet module list --all | tee -a output.txt
+echo  >> output.txt
+
+#4. Bolt Taskts
+#
+#
+
+#5. Hiera File
+#
+#
+
+#6. PQL Queries
+#
+#
