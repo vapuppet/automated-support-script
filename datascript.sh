8e8f8c349e2a3dcfa79ff51494b7a4d010dad30f
vapuppet <victor.alvarez@puppetlabs.com>
2022-04-23 14:41:14 +0000

    removed echos

--- datascript.sh
+++ datascript.sh
@@ -18,19 +18,8 @@ puppet --version | tee -a output.txt
 
 #3. PE Server Commands
 /opt/puppetlabs/puppet/bin/openssl x509 -in "$(/opt/puppetlabs/bin/puppet config print hostcert)" --enddate --noout | tee -a output.txt
-<<<<<<< HEAD
 puppet infra status | tee -a output.txt
 puppet module list --all | tee -a output.txt
-=======
-echo
-echo Puppet Infrastructure Status | tee -a output.txt
-puppet infrastructure status | tee -a output.txt
-echo
-echo Puppet Module List | tee -a output.txt
-echo "puppet module list --all" >> output.txt
-puppet module list -all | tee -a output.txt
-echo  >> output.txt
->>>>>>> 4cbe7191e86c62f76f59b4e6a5e5d71a6257458d
 
 #4. Bolt Taskts
 #
